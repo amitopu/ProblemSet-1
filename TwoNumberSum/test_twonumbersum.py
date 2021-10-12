032a236d7bae2f3411a1408895e97a6db01a0b7c
rupok <nurealamsiddiky@gmail.com>
2021-10-12 20:23:23 +0600

    new function with better time complexity is added

--- TwoNumberSum/test_twonumbersum.py
+++ TwoNumberSum/test_twonumbersum.py
@@ -1,5 +1,5 @@
 import unittest
-from twonumbersum import twoNumberSum
+from twonumbersum import twoNumberSum, twoNumberSumBetter
 
 class TestTwoNumberSum(unittest.TestCase):
 
@@ -7,6 +7,12 @@ class TestTwoNumberSum(unittest.TestCase):
 		self.assertFalse(twoNumberSum([2,3,6,4,5,7], 42))
 
 	def test_output_true(self):
+		self.assertEqual(twoNumberSum([11, -1, 3, 5, 9, 67], 10), [11, -1])
+
+	def test_output_false_better(self):
+		self.assertFalse(twoNumberSum([2,3,6,4,5,7], 42))
+
+	def test_output_true_better(self):
 		self.assertEqual(twoNumberSum([11, -1, 3, 5, 9, 67], 10), [11, -1])
 
 
--- TwoNumberSum/twonumbersum.py
+++ TwoNumberSum/twonumbersum.py
@@ -1,13 +1,60 @@
 def twoNumberSum(array, targetSum):
-    new_arr = []
-    for i in range(0, len(array)-1): #iterate over 0th to n-1 th elem
-        for j in range(i+1, len(array)): #iterate over i+1 th to n th elem
-            if array[i] + array[j] == targetSum:
-                new_arr.append(array[i])
-                new_arr.append(array[j])
-    if len(new_arr) != 0:
-        return new_arr
-    else:
-        return False
-    
-    
+	"""
+		This function takes an array of numbers and check if there exists any two numbers sum up to a target number.
+		This implementation has O(n^2) time complexity and O(1) space complexity.
+
+		args
+		---------
+		array: an array of numbers
+		targetSum: a target number
+
+		output
+		---------
+		new_array: an array of two numbers which sum up to target number
+		False: if there is no such two sumbers which sum up to target number in the input array
+
+	"""
+	new_arr = []
+	for i in range(0, len(array)-1):
+		for j in range(i+1, len(array)):
+			if(array[i] + array[j] == targetSum):
+				new_arr.append(array[i])
+				new_arr.append(array[j])
+	if len(new_arr) != 0:
+		return new_arr
+	else:
+		return False
+	
+	
+
+
+def twoNumberSumBetter(array, targetSum):
+	"""
+		This function takes an array of numbers and check if there exists any two numbers sum up to a target number.
+		This implementation has O(nlogn) time complexity and O(1) space complexity.
+
+		args
+		---------
+		array: an array of numbers
+		targetSum: a target number
+
+		output
+		---------
+		new_array: an array of two numbers which sum up to target number
+		False: if there is no such two sumbers which sum up to target number in the input array
+
+	"""
+
+	array.sort()
+
+	left = 0
+	right = len(array) - 1
+
+	if left < right:
+		if array[left] + array[right] == targetSum:
+			return [array[left], array[right]]
+		elif array[left] + array[right] < targetSum:
+			left += 1
+		elif array[left] + array[right] > targetSum:
+			right -= 1
+	return False
